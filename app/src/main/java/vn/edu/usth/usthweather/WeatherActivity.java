ed413b9f0013b84e5653cac47ac25c7cd0ccc743
caonguyen2001 <nguyennguyencao2001@gmail.com>
2021-10-27 00:57:21 +0700

    lab 7

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -2,6 +2,11 @@ package vn.edu.usth.usthweather;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -13,44 +18,88 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment ff = ForecastFragment.newInstance("","");
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.container, ff).commit();
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        Log.i("Weather", "onCreate()");
+
+        //ForecastFragment ff = ForecastFragment.newInstance("","");
+        //getSupportFragmentManager().beginTransaction().add(
+        // R.id.container, ff).commit();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("Weather","onStart() called");
+        Log.i("Weather", "onStart() called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("Weather","onStop() called");
+        Log.i("Weather", "onStop() called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i("Weather","onDestroy() called");
+        Log.i("Weather", "onDestroy() called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("Weather","onPause() called");
+        Log.i("Weather", "onPause() called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("Weather","onResume() called");
+        Log.i("Weather", "onResume() called");
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        Log.i("Weather","onCreate() called");
+        Log.i("Weather", "onCreate() called");
+    }
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[]{"Hanoi", "Paris", "Toulouse"};
+
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+
+        @Override
+        public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0:
+                    return new WeatherAndForecastFragment();
+                case 1:
+                    return new WeatherAndForecastFragment();
+                case 2:
+                    return new WeatherAndForecastFragment();
+            }
+            return new Fragment(); // failsafe
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+
+            return titles[page];
+        }
+
     }
 }
